363b777a36d0185de4a8e0117ff2b0c967659af4
deep-recommend-sugimoto <deeprecommend@gmail.com>
2022-01-15 14:25:55 +0900

    update favicon path

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
           name="description"
           content="DeepRecommend"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <CustomComponents />
     </div>
