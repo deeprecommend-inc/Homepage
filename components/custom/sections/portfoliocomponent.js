49637a3cd16473b8de20aab23e4eff845dc571c3
deep-recommend-sugimoto <deeprecommend@gmail.com>
2022-01-18 07:17:17 +0900

    change picture of portfolio

--- components/custom/sections/portfoliocomponent.js
+++ components/custom/sections/portfoliocomponent.js
@@ -9,19 +9,21 @@ import logImg1 from "../../../assets/images/portfolio/log.png";
 import logImg2 from "../../../assets/images/portfolio/log2.png";
 import logImg3 from "../../../assets/images/portfolio/log3.png";
 import logImg4 from "../../../assets/images/portfolio/log4.png";
+import logImg5 from "../../../assets/images/portfolio/deepLog.png";
 import museumImg from "../../../assets/images/portfolio/museum.jpg";
 import museumImg1 from "../../../assets/images/portfolio/museum.png";
 import museumImg2 from "../../../assets/images/portfolio/museum2.png";
 import museumImg3 from "../../../assets/images/portfolio/museum3.png";
 import museumImg4 from "../../../assets/images/portfolio/museum4.png";
+import museumImg5 from "../../../assets/images/portfolio/deepMuseum.jpg";
 import img3 from "../../../assets/images/portfolio/img3.jpg";
 import img4 from "../../../assets/images/portfolio/img4.jpg";
 import img5 from "../../../assets/images/portfolio/img5.jpg";
 import img6 from "../../../assets/images/portfolio/img6.jpg";
 import { site } from "../../../constants/const";
 
-const logStyle = {padding: "72px"}
-const museumStyle = {padding: "48px"}
+const logStyle = {padding: "0", height: '256px'}
+const museumStyle = {padding: "0", height: '256px'}
 
 const PortfolioComponent = () => {
   return (
@@ -56,7 +58,7 @@ const PortfolioComponent = () => {
                 <a style={logStyle} href={site.deepLog} target="_blank" rel="noopener noreferrer" className="img-ho">
                   <Image
                     className="card-img-top"
-                    src={logImg3}
+                    src={logImg5}
                     alt="wrappixel kit"
                   />
                 </a>
@@ -64,7 +66,7 @@ const PortfolioComponent = () => {
                   <h5 className="font-medium m-b-0">
                     DeepLog
                   </h5>
-                  <p className="m-b-0 font-14">Log and Hub of Website</p>
+                  <p className="m-b-0 font-14">Website Management</p>
                 </CardBody>
               </Card>
             </Col>
@@ -72,8 +74,8 @@ const PortfolioComponent = () => {
               <Card className="card-shadow">
                 <a style={museumStyle} href={site.deepMuseum} target="_blank" rel="noopener noreferrer" className="img-ho">
                   <Image
-                    className="card-img-top p-5"
-                    src={museumImg3}
+                    className="card-img-top"
+                    src={museumImg5}
                     alt="wrappixel kit"
                   />
                 </a>
