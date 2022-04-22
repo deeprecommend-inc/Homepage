db17f1a4d74ed1d8b0f4390c3f6f60db8b0f2746
sugimoto <sugimoto@shiftplus.co.jp>
2022-04-23 01:34:06 +0900

    google analytics

--- components/custom/sections/contactcomponent.js
+++ components/custom/sections/contactcomponent.js
@@ -1,133 +1,168 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  Row,
-  Col,
-  Container,
-  Form,
-  FormGroup,
-  Input,
-  Button,
-} from "reactstrap";
-import { smtp } from "../../../constants/const";
-import { useLocale } from "../../../locales/useLocale";
+    Row,
+    Col,
+    Container,
+    Form,
+    FormGroup,
+    Input,
+    Button,
+} from 'reactstrap';
+import { smtp } from '../../../constants/const';
+import { useLocale } from '../../../locales/useLocale';
 import Snackbar from '@mui/material/Snackbar';
+import * as gtag from '../../../utils/gtag';
 
 const ContactComponent = () => {
-  const { t } = useLocale();
-  const [snackbar, setSnackbar] = useState(false);
-  const [snackbarMes, setSnackbarMes] = useState('');
-  
-  const sendMessage = () => {
-    const name = document.getElementById('inputName').value;
-    const email = document.getElementById('inputEmail').value;
-    const message = document.getElementById('inputMessage').value;
+    const { t } = useLocale();
+    const [snackbar, setSnackbar] = useState(false);
+    const [snackbarMes, setSnackbarMes] = useState('');
 
-    Email.send({
-        Host : smtp.servername,
-        Username : smtp.username,
-        Password : smtp.password,
-        To : smtp.username,
-        From : email,
-        Subject : `${name}さんからホームページへのお問い合わせ`,
-        Body : message,
-    }).then(
-      message => openSnackbar(message)
-    );
-  }
+    const sendMessage = () => {
+        const name = document.getElementById('inputName').value;
+        const email = document.getElementById('inputEmail').value;
+        const message = document.getElementById('inputMessage').value;
 
-  const openSnackbar = (messge) => {
-    setSnackbarMes(messge);
-    setSnackbar(true);
-    setTimeout(() => {
-      setSnackbar(false);
-    }, 3000)
-  }
+        gtag.event({
+            action: 'submit_form',
+            category: 'Contact',
+            label: message,
+        });
 
-  return (
-    <div>
-      <div className="spacer bg-light">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="7" className="text-center">
-              <h1 className="title font-bold">{t.contact.form.title}</h1>
-              <h6 className="subtitle">
-                {t.contact.form.subtitle}
-              </h6>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div className="contact1">
-        <Row>
-          <Container>
-            <div className="spacer">
-              <Row className="m-0">
-                <Col lg="8">
-                  <div className="contact-box p-r-40">
-                    <h4 className="title">{t.contact.form.inner.title}</h4>
-                    <Form>
-                      <Row>
-                        <Col lg="6">
-                          <FormGroup className="m-t-15">
-                            <Input type="text" id="inputName" placeholder={t.contact.form.inner.name} />
-                          </FormGroup>
+        Email.send({
+            Host: smtp.servername,
+            Username: smtp.username,
+            Password: smtp.password,
+            To: smtp.username,
+            From: email,
+            Subject: `${name}さんからホームページへのお問い合わせ`,
+            Body: message,
+        }).then((message) => openSnackbar(message));
+    };
+
+    const openSnackbar = (messge) => {
+        setSnackbarMes(messge);
+        setSnackbar(true);
+        setTimeout(() => {
+            setSnackbar(false);
+        }, 3000);
+    };
+
+    return (
+        <div>
+            <div className="spacer bg-light">
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col md="7" className="text-center">
+                            <h1 className="title font-bold">
+                                {t.contact.form.title}
+                            </h1>
+                            <h6 className="subtitle">
+                                {t.contact.form.subtitle}
+                            </h6>
                         </Col>
-                        <Col lg="6">
-                          <FormGroup className="m-t-15">
-                            <Input type="text" id="inputEmail" placeholder={t.contact.form.inner.email} />
-                          </FormGroup>
-                        </Col>
-                        <Col lg="12">
-                          <FormGroup className="m-t-15">
-                            <Input
-                              type="textarea"
-                              name="text"
-                              id="inputMessage"
-                              placeholder={t.contact.form.inner.description}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col lg="12">
-                          <Button
-                            className="btn btn-outline-primary m-t-20 btn-arrow"
-                            onClick={sendMessage}
-                          >
-                            <span>
-                              {" "}
-                              {t.contact.form.inner.submit} <i className="ti-arrow-right"></i>
-                            </span>
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </div>
-                </Col>
-                <Col lg="4">
-                  <div className="detail-box p-40 bg-info">
-                    <h2 className="text-white">DeepRecommend</h2>
-                    <p className="text-white m-t-30 op-8">
-                      070 1734 7502
-                      <br /> deeprecommend@gmail.com
-                    </p>
-                    <p className="text-white op-8">
-                      {t.contact.detailBox.address}
-                    </p>
-                  </div>
-                </Col>
-              </Row>
+                    </Row>
+                </Container>
             </div>
-          </Container>
-        </Row>
-      </div>
-      <Snackbar
-        open={snackbar}
-        message={snackbarMes}
-        anchorOrigin={{
-          vertical: 'top', horizontal: 'center'
-        }}
-      />
-    </div>
-  );
+            <div className="contact1">
+                <Row>
+                    <Container>
+                        <div className="spacer">
+                            <Row className="m-0">
+                                <Col lg="8">
+                                    <div className="contact-box p-r-40">
+                                        <h4 className="title">
+                                            {t.contact.form.inner.title}
+                                        </h4>
+                                        <Form>
+                                            <Row>
+                                                <Col lg="6">
+                                                    <FormGroup className="m-t-15">
+                                                        <Input
+                                                            type="text"
+                                                            id="inputName"
+                                                            placeholder={
+                                                                t.contact.form
+                                                                    .inner.name
+                                                            }
+                                                        />
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col lg="6">
+                                                    <FormGroup className="m-t-15">
+                                                        <Input
+                                                            type="text"
+                                                            id="inputEmail"
+                                                            placeholder={
+                                                                t.contact.form
+                                                                    .inner.email
+                                                            }
+                                                        />
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col lg="12">
+                                                    <FormGroup className="m-t-15">
+                                                        <Input
+                                                            type="textarea"
+                                                            name="text"
+                                                            id="inputMessage"
+                                                            placeholder={
+                                                                t.contact.form
+                                                                    .inner
+                                                                    .description
+                                                            }
+                                                        />
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col lg="12">
+                                                    <Button
+                                                        className="btn btn-outline-primary m-t-20 btn-arrow"
+                                                        onClick={sendMessage}
+                                                    >
+                                                        <span>
+                                                            {' '}
+                                                            {
+                                                                t.contact.form
+                                                                    .inner
+                                                                    .submit
+                                                            }{' '}
+                                                            <i className="ti-arrow-right"></i>
+                                                        </span>
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </Form>
+                                    </div>
+                                </Col>
+                                <Col lg="4">
+                                    <div className="detail-box p-40 bg-info">
+                                        <h2 className="text-white">
+                                            DeepRecommend
+                                        </h2>
+                                        <p className="text-white m-t-30 op-8">
+                                            070 1734 7502
+                                            <br /> deeprecommend@gmail.com
+                                        </p>
+                                        <p className="text-white op-8">
+                                            {t.contact.detailBox.address}
+                                        </p>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Container>
+                </Row>
+            </div>
+            <Snackbar
+                open={snackbar}
+                message={snackbarMes}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            />
+        </div>
+    );
 };
 
 export default ContactComponent;
--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,109 @@
-import "../styles/scss/style.scss";
-import Layout from "../layout/Layout";
-import { DefaultSeo } from "next-seo";
-import Head from "next/head";
-import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import "animate.css";
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+import '../styles/scss/style.scss';
+import Layout from '../layout/Layout';
+import { DefaultSeo } from 'next-seo';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'animate.css';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { useStore } from '../store';
-import { useLocale } from "../locales/useLocale";
-import { site } from "../constants/const";
-import deeprecommendImg from "../assets/images/deeprecommend/DeepRecommend.png";
+import { useLocale } from '../locales/useLocale';
+import { site } from '../constants/const';
+import deeprecommendImg from '../assets/images/deeprecommend/DeepRecommend.png';
+import { GA_ID, existsGaId, pageview } from '../utils/gtag';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  const store = useStore()
-  const { t, locale } = useLocale();
-  const persistor = persistStore(store)
+    const store = useStore();
+    const { t, locale } = useLocale();
+    const persistor = persistStore(store);
+    const router = useRouter();
 
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      easing: "ease-out-sine",
-      duration: 600,
-    });
-  }, []);
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            easing: 'ease-out-sine',
+            duration: 600,
+        });
+    }, []);
 
-  return (
-    <Layout>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="Z6Rl8fnfOf9kvou18ZpxZmvW9HXWzt1p5uF_g35TPjw"
-        />
-      </Head>
-      <Provider store={store}>
-      <PersistGate persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </Layout>
-  );
+    useEffect(() => {
+        if (!existsGaId) {
+            return;
+        }
+
+        const handleRouteChange = (path) => {
+            pageview(path);
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
+    return (
+        <Layout>
+            <Head>
+                {/* Google Analytics */}
+                {existsGaId && (
+                    <>
+                        <script
+                            async
+                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                        />
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+                            }}
+                        />
+                    </>
+                )}
+                <meta
+                    name="google-site-verification"
+                    content="Z6Rl8fnfOf9kvou18ZpxZmvW9HXWzt1p5uF_g35TPjw"
+                />
+            </Head>
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
+                    <Component {...pageProps} />
+                </PersistGate>
+            </Provider>
+        </Layout>
+    );
 }
 
 export default MyApp;
+
+<Head>
+    {/* Google Analytics */}
+    {existsGaId && (
+        <>
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+                }}
+            />
+        </>
+    )}
+</Head>;
