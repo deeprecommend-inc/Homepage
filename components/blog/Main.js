c77c3aeca999d2472fb3636762aea33809ffd3a5
root <root@DESKTOP-MGOMRR6.localdomain>
2022-08-15 07:14:37 +0900

    blog arrow back button

--- components/blog/Main.js
+++ components/blog/Main.js
@@ -7,100 +7,100 @@ import * as marked from 'marked';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
 import { useDispatch } from 'react-redux';
-import { blogDetailSlice } from "../../store/blogDetail";
+import { blogDetailSlice } from '../../store/blogDetail';
 
 function Main(props) {
-  const { detail, title } = props;
-  const [ready, setReady] = useState(false);
-  const [html, setHtml] = useState('');
-  const [speakingInfo, setSpeakingInfo] = useState();
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    setReady(true);
-    parse();
-    genToc();
-    setInitSpeakingInfo();
-  }, [])
+    const { detail, title } = props;
+    const [ready, setReady] = useState(false);
+    const [html, setHtml] = useState('');
+    const [speakingInfo, setSpeakingInfo] = useState();
+    const dispatch = useDispatch();
 
-  const parse = () => {
-    marked.setOptions({
-      renderer: new marked.Renderer(),
-      highlight: (code, lang) => {
-        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-        return hljs.highlight(code, { language }).value;
-      },
-      langPrefix: 'hljs language-',
-      pedantic: false,
-      gfm: true,
-      breaks: false,
-      sanitize: false,
-      smartLists: true,
-      smartypants: false,
-      xhtml: false
-    });
+    useEffect(() => {
+        setReady(true);
+        parse();
+        genToc();
+        setInitSpeakingInfo();
+    }, []);
 
-    const parsed = marked.parse(detail);
-    setHtml(parsed);
-  }
+    const parse = () => {
+        marked.setOptions({
+            renderer: new marked.Renderer(),
+            highlight: (code, lang) => {
+                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                return hljs.highlight(code, { language }).value;
+            },
+            langPrefix: 'hljs language-',
+            pedantic: false,
+            gfm: true,
+            breaks: false,
+            sanitize: false,
+            smartLists: true,
+            smartypants: false,
+            xhtml: false,
+        });
 
-  const genToc = () => {
-    const toc = []
-    const renderer = new marked.Renderer()
-    renderer.heading = (text, level) => {
-      const slug = encodeURI(text.toLowerCase())
-      toc.push({
-        level: level,
-        slug: slug,
-        title: text
-      })
-      return '<h'
-        + level
-        + ' id="'
-        + slug
-        + '">'
-        + text
-        + '</h'
-        + level
-        + '>\n'
-    }
-    marked.setOptions({ renderer: renderer })
-    marked.parse(detail)
-    dispatch(
-      blogDetailSlice.actions.setToc(toc)
-    )
-  }
+        const parsed = marked.parse(detail);
+        setHtml(parsed);
+    };
 
-  const setInitSpeakingInfo = () => {
-    const speak = new SpeechSynthesisUtterance();
-    speak.text = html
-    speak.lang = 'en-US'
-    setSpeakingInfo(speak);
-  }
+    const genToc = () => {
+        const toc = [];
+        const renderer = new marked.Renderer();
+        renderer.heading = (text, level) => {
+            const slug = encodeURI(text.toLowerCase());
+            toc.push({
+                level: level,
+                slug: slug,
+                title: text,
+            });
+            return (
+                '<h' +
+                level +
+                ' id="' +
+                slug +
+                '">' +
+                text +
+                '</h' +
+                level +
+                '>\n'
+            );
+        };
+        marked.setOptions({ renderer: renderer });
+        marked.parse(detail);
+        dispatch(blogDetailSlice.actions.setToc(toc));
+    };
 
-  if (!ready) return <></>
+    const setInitSpeakingInfo = () => {
+        const speak = new SpeechSynthesisUtterance();
+        speak.text = html;
+        speak.lang = 'en-US';
+        setSpeakingInfo(speak);
+    };
 
-  return (
-    <Grid
-      item
-      xs={12}
-      md={8}
-      sx={{
-        '& .markdown': {
-          py: 3,
-        },
-        background: "#ffffff",
-        borderRadius: "20px"
-      }}
-    >
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      <Divider />
-      <div dangerouslySetInnerHTML={{__html: html}} />
-      <style src='highlightjs/styles/github-gist.css'></style>
-    </Grid>
-  );
+    if (!ready) return <></>;
+
+    return (
+        <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{
+                '& .markdown': {
+                    py: 3,
+                },
+                background: '#ffffff',
+                borderRadius: '20px',
+            }}
+        >
+            <Typography variant="h6" gutterBottom>
+                {title}
+            </Typography>
+            <Divider />
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <style src="highlightjs/styles/github-gist.css"></style>
+        </Grid>
+    );
 }
 
 export default Main;
--- components/blog/MainFeaturedPost.js
+++ components/blog/MainFeaturedPost.js
@@ -5,55 +5,80 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
+import { IconButton } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useRouter } from 'next/router';
 
 function MainFeaturedPost(props) {
-  const { detail } = props;
+    const { detail } = props;
+    const router = useRouter();
 
-  return (
-    <Paper
-      sx={{
-        position: 'relative',
-        backgroundColor: 'grey.800',
-        color: '#fff',
-        mb: 4,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundImage: `url(${detail.image})`,
-      }}
-    >
-      {/* Increase the priority of the hero background image */}
-      {<img style={{ display: 'none' }} src={detail.image} alt={detail.imageText} />}
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          backgroundColor: 'rgba(0,0,0,.3)',
-        }}
-      />
-      <Grid container>
-        <Grid item md={6}>
-          <Box
+    const backToBlockList = () => {
+        router.push('/lab');
+    };
+
+    return (
+        <Paper
             sx={{
-              position: 'relative',
-              p: { xs: 3, md: 6 },
-              pr: { md: 0 },
+                position: 'relative',
+                backgroundColor: 'grey.800',
+                color: '#fff',
+                mb: 4,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: `url(${detail.image})`,
             }}
-          >
-            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-              {detail.title}
-            </Typography>
-            <Typography variant="h5" color="inherit" paragraph>
-              {detail.description}
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
+        >
+            {/* Increase the priority of the hero background image */}
+            {
+                <img
+                    style={{ display: 'none' }}
+                    src={detail.image}
+                    alt={detail.imageText}
+                />
+            }
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0,0,0,.3)',
+                }}
+            />
+            <Grid container>
+                <Grid item md={6}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            p: { xs: 3, md: 6 },
+                            pr: { md: 0 },
+                        }}
+                    >
+                        <IconButton
+                            sx={{ marginBottom: '24px' }}
+                            onClick={backToBlockList}
+                        >
+                            <ArrowBackIosIcon className="text-white" />
+                        </IconButton>
+                        <Typography
+                            component="h1"
+                            variant="h3"
+                            color="inherit"
+                            gutterBottom
+                        >
+                            {detail.title}
+                        </Typography>
+                        <Typography variant="h5" color="inherit" paragraph>
+                            {detail.description}
+                        </Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
 }
 
 export default MainFeaturedPost;
